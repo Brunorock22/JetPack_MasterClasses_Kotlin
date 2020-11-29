deece163eb0cc079b82cf93a0161422d309bd2c3
Bruno Camargos <brunocamargosn@gmail.com>
2020-11-29 19:04:58 -0300

    DAta biding

--- app/src/main/java/com/example/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/example/dogs/view/DetailFragment.kt
@@ -1,32 +1,33 @@
 package com.example.dogs.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.NavDirections
-import androidx.navigation.Navigation
 import com.example.dogs.R
-import com.example.dogs.util.getProgressDrawable
-import com.example.dogs.util.loadImage
+import com.example.dogs.databinding.FragmentDetailBinding
 import com.example.dogs.viewmodel.DetailViewModel
 import kotlinx.android.synthetic.main.fragment_detail.*
-import kotlinx.android.synthetic.main.item_dog.view.*
 
 
 class DetailFragment : Fragment() {
 
     private lateinit var viewModel: DetailViewModel
     private var dogId: Int = 0
+    lateinit var binding: FragmentDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,12 +48,13 @@ class DetailFragment : Fragment() {
             dog?.let {
                 dogContainer.visibility = View.VISIBLE
                 loadingViewDetail.visibility = View.GONE
+                binding.dogDataBidingDetails = it
 
-                dogName.text = it.dogBreed
-                dogPurpose.text = it.bredFor
-                dogTemperament.text = it.temperament
-                dogLifeStyle.text = it.lifeSpan
-                dogImageDetails.loadImage(it.imgUrl,  getProgressDrawable(dogImageDetails.context))
+//                dogName.text = it.dogBreed
+//                dogPurpose.text = it.bredFor
+//                dogTemperament.text = it.temperament
+//                dogLifeStyle.text = it.lifeSpan
+//                dogImageDetails.loadImage(it.imgUrl, getProgressDrawable(dogImageDetails.context))
             }
 
         })
--- app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
+++ app/src/main/java/com/example/dogs/view/DogsListAdapter.kt
@@ -3,46 +3,45 @@ package com.example.dogs.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dogs.R
+import com.example.dogs.databinding.ItemDogBinding
 import com.example.dogs.model.Dog
-import com.example.dogs.util.getProgressDrawable
-import com.example.dogs.util.loadImage
 import kotlinx.android.synthetic.main.item_dog.view.*
 
 class DogsListAdapter(val dogsList: ArrayList<Dog>) :
-    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+    RecyclerView.Adapter<DogsListAdapter.DogViewHolder>(), DogClickListener {
     fun updateDogList(newDogsList: List<Dog>) {
         dogsList.clear()
         dogsList.addAll(newDogsList)
         notifyDataSetChanged()
     }
 
-    inner class DogViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-    }
+    class DogViewHolder(var view: ItemDogBinding) : RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        // val view = LayoutInflater.from(parent.context).inflate(R.layout.item_dog, parent, false) as View
         val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_dog, parent, false) as View
+            DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
         return DogViewHolder(view)
 
     }
 
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
-        holder.itemView.name.text = dogsList[position].dogBreed
-        holder.itemView.lifespan.text = dogsList[position].lifeSpan
-        holder.itemView.imageView.loadImage(
-            dogsList[position].imgUrl,
-            getProgressDrawable(holder.itemView.imageView.context)
-        )
-        holder.itemView.setOnClickListener {
-            val action = ListFragmentDirections.actionDetailFragment()
-            action.dogUuid = dogsList[position].uui
-            Navigation.findNavController(it).navigate(action)
-        }
+        holder.view.dogDataBiding = dogsList[position]
+        holder.view.listenerClicked = this
+
     }
 
     override fun getItemCount(): Int = dogsList.count()
+
+    override fun onDogClicked(view: View) {
+        val uuid = view.dogId.text.toString().toInt()
+        val action = ListFragmentDirections.actionDetailFragment()
+        action.dogUuid = uuid
+        Navigation.findNavController(view).navigate(action)
+    }
 }
